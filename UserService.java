7fbc59495f43deae0b5232584069e5a064ee7488
maruti <marutikphani@gmail.com>
2025-04-08 22:40:14 -0500

    Add login method

--- UserService.java
+++ UserService.java
@@ -2,4 +2,8 @@ public class UserService {
     public String getUserName() {
         return "John Doe";
     }
+
+    public String login (String userName, String password){
+        return "Login successfull for: "+ userName;
+    }
 }
